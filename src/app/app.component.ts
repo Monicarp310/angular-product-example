d40abcc34ddb14ff0f536e6f141751428ee7fb65
Mónica Rodríguez Pérez <monicarodriguez@devanddel.com>
2022-02-22 17:46:14 +0100

    calcular producto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Product } from './model/product.model';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,37 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-product-example';
+
+  products: Product[] = [
+    {
+      code: "457hkns8",
+      name: "Pack palmeritas",
+      price: 1.80,
+      units: 2,
+    },
+    {
+      code: "12ti5n5g",
+      name: "Brick leche",
+      price: 1.20,
+      units: 6,
+    },
+    {
+      code: "2151ead5",
+      name: "Botella 2L Coca Cola",
+      price: 2.50,
+      units: 1,
+    }
+  ];
+
+  constructor(){
+    console.log(this.getTotalPrice())
+  }
+
+  getTotalPrice(): number {
+    let total = 0;
+    for(let products of this.products){
+      total = products.units * products.price
+    }
+  return total;
+  }
 }
